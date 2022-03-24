78b966470098423b0331d83dd5ebff8fbb9592db
peterdt123 <44677100+peterdt123@users.noreply.github.com>
2022-03-24 16:08:35 -0700

    initial commit 2

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -1,8 +1,8 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import { CodePipeLine, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
-import { ManualAppovalStep } from 'aws-cdk-lib/pipelines';
-import { MyPipelineAppStage } from './stage';
+import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
+//import { MyPipelineAppStage } from './stage';
 import { pipeline } from 'stream';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
@@ -11,7 +11,7 @@ export class CiCdAwsPipelineDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeLine(this, 'Pipeline', {
+    new CodePipeline(this, 'Pipeline', {
       pipelineName : 'TestPineline',
       synth : new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('peterdt123/AWS-CodePipeline', 'main'),
