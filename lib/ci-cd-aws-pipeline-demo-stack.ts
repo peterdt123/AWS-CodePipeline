5369006cbd962fe1fdb4f9f6b68d50cfcfcf07d9
peterdt123 <44677100+peterdt123@users.noreply.github.com>
2022-03-24 17:57:07 -0700

    initializing lambda

--- lib/ci-cd-aws-pipeline-demo-stack.ts
+++ lib/ci-cd-aws-pipeline-demo-stack.ts
@@ -2,8 +2,8 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { ManualApprovalStep } from 'aws-cdk-lib/pipelines';
-//import { MyPipelineAppStage } from './stage';
 import { pipeline } from 'stream';
+import { MyPipelineAppStage } from './stage';
 
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
@@ -11,7 +11,7 @@ export class CiCdAwsPipelineDemoStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'Pipeline', {
+    const pipeline = new CodePipeline(this, 'Pipeline', {
       pipelineName : 'TestPineline',
       synth : new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('peterdt123/AWS-CodePipeline', 'main'),
@@ -20,5 +20,16 @@ export class CiCdAwsPipelineDemoStack extends Stack {
                    'npx cdk synth']
       }),
     });
+
+    const testingStage = pipeline.addStage(new MyPipelineAppStage(this, "test", {
+      env: {account: "056093334675", region: "us-west-2"}
+    }));
+
+    testingStage.addPost(new ManualApprovalStep('Manual approval before production'));
+
+    const prodStage = pipeline.addStage(new MyPipelineAppStage(this, "prod", {
+      env: {account: "056093334675", region: "us-west-2"}
+    }));
+
   }
 }
